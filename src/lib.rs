cb623d3f87ef1fb92dbd0545a15916f56e2ef836
Adrián Ramos <adrianramos@stratio.com>
2025-06-17 19:18:25 +0200

    Remove old implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,6 @@
-pub mod api {
-    pub mod controller;
-}
-pub mod application {
-    pub mod actor {
-        pub mod collection;
-        pub mod sbom;
-    }
-    pub mod bus {
-        pub mod event;
-    }
-}
+pub mod application;
 pub mod config;
 pub mod domain {
     pub mod collection;
     pub mod component;
-}
-pub mod infrastructure {
-    pub mod generator {
-        pub mod sbom {
-            pub mod syft;
-        }
-        pub mod vulnerability {
-            pub mod grype;
-        }
-    }
 }
